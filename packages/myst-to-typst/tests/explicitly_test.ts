7178583287768be54ed51dbb7e69834b54fbc63e
Jan David Fischbach <mail@jan-dav.id>
2026-01-06 21:22:14 +0100

    first tests of status quo

---
+++ packages/myst-to-typst/tests/explicitly_test.ts
@@ -0,0 +1,23 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import yaml from 'js-yaml';
+import { unified } from 'unified';
+import mystToTypst from '../src/index.ts';
+
+// Read YAML file
+const content = fs.readFileSync(
+  path.join('tests/sidebyside.yml'),
+  { encoding: 'utf-8' }
+);
+
+// Parse YAML
+const cases = yaml.load(content) as unknown;
+
+const mdast = cases.cases[0].mdast;
+
+const pipe: any = unified().use(mystToTypst);
+
+pipe.runSync(mdast);
+const file = pipe.stringify(mdast);
+
+console.log(file);
